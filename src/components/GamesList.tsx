861770338ac35fce50f9b40736e2a5d73af26152
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-26 15:30:14 -0400

    styles added

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -5,11 +5,12 @@ import GamesCard from "./card/GamesCard";
 const GamesList = async () => {
   const gamesData = await fetchGames();
   return (
-    <div>
-        <h1 className="text-2xl font-bold p-4 ">All the games</h1>
+    <div className="h-fit">
+      <h1 className="text-2xl font-bold p-4 ">All the games</h1>
       {gamesData.results.map((game: games) => (
         <GamesCard gamesData={game} />
       ))}
+      <p>pagination</p>
     </div>
   );
 };
--- src/components/card/GamesCard.tsx
+++ src/components/card/GamesCard.tsx
@@ -1,25 +1,36 @@
+"use client";
+
 import { gamesCardProps } from "@/types";
 import Image from "next/image";
-
+import { useState } from "react";
 
 const GamesCard = ({ gamesData }: gamesCardProps) => {
-  console.log("GAMES DATA", gamesData);
-  return (
-    <section className="bg-gray-700 rounded-md mb-6 overflow-hidden">
-      
-        <figure className="w-full h-[12.5rem]">
-          <Image
-            src={gamesData.background_image}
-            alt={gamesData.name}
-            className="w-full h-full"
-            width={300}
-            height={350}
-            objectFit="contain"
-          />
-        </figure>
-        <h4 className="text-white">{gamesData.name}</h4>
+  const [moreDetailsShowed, setMoreDetailsShowed] = useState<boolean>(false);
 
-      
+  return (
+    <section className={`w-full h-auto bg-gray-700 rounded-md mb-6 overflow-hidden transition-all ${moreDetailsShowed ? 'z-20' : 'z-0'}`}>
+      <figure className="w-full h-[12.5rem]">
+        <Image
+          src={gamesData.background_image}
+          alt={gamesData.name}
+          className="w-full h-full"
+          width={300}
+          height={350}
+          objectFit="contain"
+        />
+      </figure>
+      <h4 className="text-white">{gamesData.name}</h4>
+      <button onClick={()=> setMoreDetailsShowed(!moreDetailsShowed)}>{moreDetailsShowed ? "view less..." : "view more..."}</button>
+      {moreDetailsShowed && (
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. At impedit
+            animi nam voluptates. Repellat ipsam iste rem facere, ducimus
+            expedita! Repudiandae libero ducimus, esse voluptatem quibusdam ab
+            quaerat placeat totam.
+          </p>
+        </div>
+      )}
     </section>
   );
 };
