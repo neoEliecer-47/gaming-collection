b90943c18f4e749c6f1083bdebb9612b9b2b5c5c
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-29 10:10:22 -0400

    arrow icons added and more optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['media.rawg.io', '*'],
+        domains: ['media.rawg.io'],
     }
 };
 
--- src/components/card/SliderImages.tsx
+++ src/components/card/SliderImages.tsx
@@ -3,6 +3,8 @@
 import { sliderImagesProps } from "@/types";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import ArrowLeftIcon from "../icons/ArrowPrev";
+import ArrowRightIcon from "../icons/ArrowNext";
 
 const SliderImages = ({ images }: sliderImagesProps) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
@@ -27,38 +29,44 @@ const SliderImages = ({ images }: sliderImagesProps) => {
   return (
     <div className="relative overflow-hidden h-full w-full ">
       <button
-        className="absolute text-purple-600 top-[50%] z-10"
+        className="absolute bg-white/15 top-[50%] z-10 ml-2 p-1 rounded-full"
         onClick={handlePrevImage}
       >
-        pre
+       <ArrowLeftIcon />
       </button>
       <div className="flex transition-all w-full h-full">
         {images.map(({ image, id }, index) => (
           <>
+            {isImageNotLoaded && (
+              <div>loading...</div>
+            )}
             <Image
               key={id}
               src={image}
               alt="image"
               width={300}
               height={350}
+              quality={40}
               className={`w-full h-auto aspect-video transition-all duration-300 ${
                 isImageNotLoaded
                   ? "opacity-0 translate-y-2"
                   : "opacity-100 translate-y-0"
               } ${index !== currentIndex && "hidden"}`}
               objectFit="contain"
-              priority={index === 0}
-              loading="eager"
+              priority={index === 0}//only preload the very first image
+              loading='eager'//only load eagerly the first four images and lazy the rest ones
               onLoadingComplete={handleImageLoad}
+              
+             
             />
           </>
         ))}
       </div>
       <button
-        className="absolute text-purple-600 right-0 top-[50%] z-10"
+        className="absolute bg-white/15 right-0 top-[50%] z-10 mr-2 p-1 rounded-full"
         onClick={handleNextImage}
       >
-        nxt
+       <ArrowRightIcon />
       </button>
     </div>
   );
