643a3b9e3a4e087612086aee332fe75189eed334
Eliecer <eliecer.sanchez47@gmail.com>
2025-01-31 15:18:10 -0400

    fixed background for game details done

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -3,17 +3,17 @@ import { games } from "@/types";
 import GamesCard from "./card/GamesCard";
 import Pagination from "./Pagination";
 
-const GamesList = async ({ currentPage }: {currentPage: number}) => {
+const GamesList = async ({ currentPage }: { currentPage: number }) => {
   const gamesData = await fetchGames(currentPage);
   return (
     <div className="h-fit">
       <h1 className="text-2xl font-bold p-4 ">All the games</h1>
-    <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-0 m-auto">
-    {gamesData.results.map((game: games) => (
-        <GamesCard gamesData={game} />
-      ))}
-    </div>
-      <Pagination currentPage={currentPage} totalPages={40}/>
+      <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-0 m-auto">
+        {gamesData.results.map((game: games) => (
+          <GamesCard gamesData={game} />
+        ))}
+      </div>
+      <Pagination currentPage={currentPage} totalPages={40} />
     </div>
   );
 };
@@ -21,3 +21,8 @@ const GamesList = async ({ currentPage }: {currentPage: number}) => {
 export default GamesList;
 
 export const dynamic = "force-static"; //ensures that the page is statically generated at build time.
+
+// <div
+//  
+//   style={{height: 300px; background-color: transparent, background-image: linear-gradient(rgba(15, 15, 15, 0), rgb(21, 21, 21)), linear-gradient(rgba(21, 21, 21, 0.8), rgba(21, 21, 21, 0.5)), url(`${}`), z-index: 1}}
+// ></div>;
--- src/components/game/GameDetails.tsx
+++ src/components/game/GameDetails.tsx
@@ -1,23 +1,27 @@
 import { gameDetailsProps } from "@/types";
 import Image from "next/image";
 import Header from "../Header";
-import styles from './GameDetails.module.css'
+import styles from "./GameDetails.module.css";
 import classNames from "classnames";
 
 const GameDetails = ({ gameDetailsData }: gameDetailsProps) => {
   return (
-    <div className="relative z-[2]">
+    <div className="relative z-[2] overflow-y-hidden">
       <Header />
-      <Image
-        src={gameDetailsData.background_image}
-        alt="bg"
-        width={500}
-        height={700}
-        className={classNames('top-[-16rem] md:top-[-5.5rem] lg:top-[-1rem] left-0 z-0 brightness-[0.3] md:brightness-[0.25] object-contain md:object-cover', styles.image)}
-        
+
+      <div
+        style={{
+          backgroundImage: `linear-gradient(rgba(15, 15, 15, 0), rgb(21, 21, 21)), linear-gradient(rgba(21, 21, 21, 0.3), rgba(21, 21, 21, 0.9)), url(${gameDetailsData.background_image})`,
+        }}
+        className="fixed bg-transparent w-full h-[15rem] top-0 md:h-full  left-0 object-cover bg-contain bg-no-repeat z-[2] md:object-cover md:bg-cover border-none"
       />
-      <div className={styles.overlay}/>
-      <h1 className="relative text-white text-lg font-bold">{gameDetailsData.name}</h1>
+
+      <div className="fixed h-full w-full bg-[rgba(21,21,21)]"></div>
+
+      <h1 className="relative text-white text-lg font-bold z-[2]">
+        {gameDetailsData.name}
+      </h1>
+      
     </div>
   );
 };
