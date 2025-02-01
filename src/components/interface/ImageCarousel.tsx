abd096acd15a3f94eaf385e15b3fbae833655342
Eliecer <eliecer.sanchez47@gmail.com>
2025-02-01 15:41:21 -0400

    carousel done

--- src/components/interface/ImageCarousel.tsx
+++ src/components/interface/ImageCarousel.tsx
@@ -1,35 +1,57 @@
-'use client'
+"use client";
 
-import { imageCarousel, screenshots } from "@/types"
-import Image from "next/image"
-import { useEffect, useState } from "react"
+import { imageCarousel, screenshots } from "@/types";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const ImageCarousel = ({ images }: imageCarousel) => {
-    const [currentIndex, setCurrentIndex] = useState<number>(0)
-console.log('first', images)
-    useEffect(()=>{
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
-        }, 5000);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  console.log("first", images);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
 
-        return ()=> clearInterval(interval)
-    },[images.length])
-  
-  
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
-    <div className="relative w-full h-[300px] overflow-hidden">
-        <div style={{ transform: `translateX(-${currentIndex * 100}%)` }} className="flex transition-all duration-500">
-            {images.map(({ id, image }, index)=> (
-                <div key={id} className="min-w-full relative">
-                    <Image src={image} alt='asas' width={300} height={350} priority={index === 0} className="object-cover w-full h-auto"/>
-                </div>
-            ))}
-        </div>
+    <div className="relative w-full h-[300px] overflow-hidden mt-4 ">
+      <div
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        className="flex transition-all duration-500"
+      >
+        {images.map(({ id, image }, index) => (
+          <div
+            key={id}
+            className="min-w-full h-[13rem] relative border-white/40 border-y-[1px]"
+          >
+            <Image
+              src={image}
+              alt="asas"
+              fill
+              priority={index < 3}
+              className="object-cover w-full h-full"
+              loading="eager"
+            />
+          </div>
+        ))}
+      </div>
 
-
-        <section>dots here...</section>
+      <section className="absolute bottom-[20%] w-full flex items-center justify-center gap-2 ">
+        {images.map((_, index) => (
+          <button
+            onClick={() => setCurrentIndex(index)}
+            key={index}
+            className={`transition-all h-3 w-3 rounded-full ${
+              currentIndex === index ? "bg-white" : "bg-gray-500"
+            }`}
+            style={{ boxShadow: currentIndex === index ? '0px 0px 5px 1px #fff' : '' }}
+          />
+        ))}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default ImageCarousel
+export default ImageCarousel;
