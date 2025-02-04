476f6de0574cb4b2038c9d27ef5304c2f1ae5bee
Eliecer <eliecer.sanchez47@gmail.com>
2025-02-04 11:08:57 -0400

    read more function completed

--- src/components/game/GameAbout.tsx
+++ src/components/game/GameAbout.tsx
@@ -1,12 +1,27 @@
+'use client'
+
+import { useRef, useState } from "react";
+
+
 const GameAbout = ({ description }: { description: string }) => {
+  const [hiddenText, setHiddenText] = useState<boolean>(true)
+  const aboutRef = useRef<HTMLDivElement>(null)
   return (
-    <div className="w-full h-[20rem] overflow-hidden">
+    <div className="w-full h-fit overflow-hidden" onClick={()=>setHiddenText(!hiddenText)}>
       <h1 className="text-white text-center text-2xl font-bold">About</h1>
       <div
-        className="text-white"
-        dangerouslySetInnerHTML={{ __html: description.slice(0, 50) }}
-      />
-      
+        ref={aboutRef}
+        className={`text-white bg-black`}
+        style={{ 
+          height: hiddenText
+            ? "0px"
+            : `${aboutRef.current?.scrollHeight}px`,
+        }}
+        
+      >
+      {/* <div className="text-white" dangerouslySetInnerHTML={{ __html: description }} />  */}
+      <div dangerouslySetInnerHTML={{ __html: description }} />
+      </div>
     </div>
   );
 };
--- src/components/game/GameDetails.tsx
+++ src/components/game/GameDetails.tsx
@@ -10,36 +10,41 @@ import MultimediaGameContent from "./MultimediaGameContent";
 import { fetchGameImages } from "@/helpers";
 import GameAbout from "./GameAbout";
 
-const GameDetails =async ({ gameDetailsData }: gameDetailsProps) => {
- const imagesData = await fetchGameImages(gameDetailsData?.id)
+const GameDetails = async ({ gameDetailsData }: gameDetailsProps) => {
+  const imagesData = await fetchGameImages(gameDetailsData?.id);
   return (
-    <div className="relative w-full min-h-[100vh] bg-black px-2">
+    <div className="relative w-full min-h-screen px-2 overflow-y-hidden">
+
+      <div className="absolute inset-0 w-full h-[500px] z-[-1]">
+        <Image
+          src={gameDetailsData.background_image}
+          alt="image"
+          quality={40}
+          layout="fill"
+          priority
+          objectFit="cover"
+          className=" bg-cover"
+          loading="eager"
+        />
+      </div>
       <Header />
       <section className="relative text-white p-2 flex justify-center items-center z-[2] gap-2">
-        <Link href='/'>home</Link>
+        <Link href="/">home</Link>
         <span>/</span>
-        <Link href='/'>games</Link>
+        <Link href="/">games</Link>
       </section>
 
       <section className="relative z-[2] flex justify-center gap-4 items-center m-auto ">
-        <span className="text-white">{buildDate(gameDetailsData.released)}</span>
-        
-        <PlatformCardImages platforms={gameDetailsData.parent_platforms}/>
-      </section>
+        <span className="text-white">
+          {buildDate(gameDetailsData.released)}
+        </span>
 
-      <Image
-        src={gameDetailsData.background_image}
-        alt="image"
-        quality={40}
-        fill
-        priority
-        className="object-cover"
-        loading="eager"
-      />
+        <PlatformCardImages platforms={gameDetailsData.parent_platforms} />
+      </section>
 
       {/* <div className="fixed h-full w-full bg-[rgba(21,21,21)]"></div> */}
       <div
-        className="absolute top-0 left-0 w-full h-full z-[1]"//change to 1
+        className="absolute top-0 left-0 w-full h-full z-[-1]" //change to 1
         style={{
           background:
             "linear-gradient(rgba(15, 15, 15, 0.5), rgb(21, 21, 21)), linear-gradient(rgba(21, 21, 21, 0.3), rgba(21, 21, 21, 0.9))",
@@ -52,12 +57,18 @@ const GameDetails =async ({ gameDetailsData }: gameDetailsProps) => {
         </h1>
       </div>
 
-      
-        <MultimediaGameContent id={gameDetailsData.id} images={imagesData.results}/>
-      
+      <MultimediaGameContent
+        id={gameDetailsData.id}
+        images={imagesData.results}
+      />
 
-      <div className="relative z-[2] w-full h-auto"> 
-        <GameAbout description={gameDetailsData.description}/>
+      <div className="relative z-[2] w-full">
+        <GameAbout description={gameDetailsData.description} />
+      </div>
+
+
+      <div className="relative z-[2] w-full p-16 mt-[2rem] bg-white/40">
+        <h3>more content here</h3>
       </div>
     </div>
   );
