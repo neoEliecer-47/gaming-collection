b57356e7d8a32eb144e4d1e26a5578857516d0ba
Eliecer <eliecer.sanchez47@gmail.com>
2025-02-08 18:57:52 -0400

    searched Games overflow fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import GamesList from "@/components/GamesList";
 import Header from "@/components/Header";
+import { searchParamsProps } from "@/types";
 
 import { Suspense } from "react";
 
-export default async function Home({ searchParams }: { searchParams: { query: string } }) {
+export default async function Home({ searchParams }: searchParamsProps) {
+ const params = await searchParams;
+  console.log('homeeeeeeeeeeee',params)
   return (
     <>
-   <Header searchParams={searchParams.query}/>
+   <Header searchParams={(await searchParams).query}/>
     <main className="bg-black/20 px-2 overflow-x-hidden">
       
       <Suspense fallback={<p>Loading...</p>}>
