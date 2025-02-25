86b202929ae4dceb019f17e805508ca10a061b1a
Eliecer <eliecer.sanchez47@gmail.com>
2025-02-25 14:59:21 -0400

    read more component updated

--- src/components/game/GameReadMore.tsx
+++ src/components/game/GameReadMore.tsx
@@ -2,24 +2,43 @@
 
 import { useRef, useState } from "react";
 
-const GameReadMore = ({ description }: { description: string }) => {
+const GameReadMore = ({
+  description,
+  title,
+  initialHeight = 7.5,
+  dangerousHtml = true,
+}: {
+  description: string;
+  title?: string;
+  initialHeight: number;
+  dangerousHtml?: boolean;
+}) => {
   const [hiddenText, setHiddenText] = useState<boolean>(true);
-  const aboutRef = useRef<HTMLDivElement>(null);
+  const readMoreRef = useRef<HTMLDivElement>(null);
   return (
     <div
       className="w-full h-fit overflow-hidden"
       onClick={() => setHiddenText(!hiddenText)}
     >
-      <h1 className="text-white text-center text-2xl font-bold">About</h1>
+      <h1 className="text-white text-center text-2xl font-bold">{title}</h1>
       <div
-        ref={aboutRef}
+        ref={readMoreRef}
         className={`text-white bg-black`}
         style={{
-          height: hiddenText ? "7.5rem" : `${aboutRef.current?.scrollHeight}px`,
+          height: hiddenText
+            ? `${initialHeight}rem`
+            : `${readMoreRef.current?.scrollHeight}px`,
         }}
       >
         {/* <div className="text-white" dangerouslySetInnerHTML={{ __html: description }} />  */}
-        <div dangerouslySetInnerHTML={{ __html: description }} />
+        {dangerousHtml ? (
+          <div
+            className="text-white"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        ) : (
+          <p className="text-white">{description}</p>
+        )}
       </div>
       <div className="p-0 m-0 flex items-center justify-end">
         <button className="text-white bg-green-500">
