15a22299ef0beed2ee97a8aeee4bdf7b87675c07
Eliecer <eliecer.sanchez47@gmail.com>
2025-02-25 20:31:08 -0400

    read more functionality done

--- src/components/GameTitleInfo.tsx
+++ src/components/GameTitleInfo.tsx
@@ -1,23 +1,32 @@
-import { fetchPlatforms } from "@/helpers"
-import GameAbout from "./game/GameReadMore"
+import { fetchPlatforms } from "@/helpers";
+import GameReadMore from "./game/GameReadMore";
 
 type dataProps = {
-    name: string;
-    description: string;
-}
+  name: string;
+  description: string;
+};
 
-const GameTitleInfo =async ({ searchParams }: { searchParams: Record<string, string> }) => {
+const GameTitleInfo = async ({
+  searchParams,
+}: {
+  searchParams: Record<string, string>;
+}) => {
+  const data = await fetchPlatforms(searchParams.platforms);
 
-      const data = await fetchPlatforms(searchParams.platforms)
-    
   return (
     <article>
-   
-             <h1>{data.name}</h1>
-             <GameAbout description={data.description}/>
-  
+      {data.description ? (
+        <GameReadMore
+          description={data.description}
+          title={data.name}
+          initialHeight={5}
+          duration={300}
+        />
+      ) : (
+        <h1>{data.name}</h1>
+      )}
     </article>
-  )
-}
+  );
+};
 
-export default GameTitleInfo
+export default GameTitleInfo;
--- src/components/game/GameReadMore.tsx
+++ src/components/game/GameReadMore.tsx
@@ -1,50 +1,80 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const GameReadMore = ({
   description,
   title,
   initialHeight = 7.5,
   dangerousHtml = true,
+  duration = 300,
+  maxLength = 120
 }: {
   description: string;
   title?: string;
   initialHeight: number;
   dangerousHtml?: boolean;
+  duration?: number,
+  maxLength?: number
 }) => {
   const [hiddenText, setHiddenText] = useState<boolean>(true);
+  const [dynamicHeight, setDynamicHeight] = useState('75px')
   const readMoreRef = useRef<HTMLDivElement>(null);
+
+  const truncatedText = description.slice(0, maxLength);
+  const shouldTruncate = description.length > maxLength;
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setDynamicHeight(hiddenText ? `75px` : `${readMoreRef.current?.scrollHeight}px`)
+    }, 0)
+  }, [hiddenText])
+
   return (
     <div
-      className="w-full h-fit overflow-hidden"
-      onClick={() => setHiddenText(!hiddenText)}
+      className="w-full h-fit overflow-hidden mb-4"
+      onClick={() => !hiddenText && setHiddenText(!hiddenText)}
     >
       <h1 className="text-white text-center text-2xl font-bold">{title}</h1>
       <div
         ref={readMoreRef}
-        className={`text-white bg-black`}
+        className={`relative inline-flex justify-center text-white w-full bg-black transition-all duration-${duration} ease-in-out overflow-hidden`}
         style={{
-          height: hiddenText
-            ? `${initialHeight}rem`
-            : `${readMoreRef.current?.scrollHeight}px`,
+          height: dynamicHeight
+           
+         
         }}
       >
-        {/* <div className="text-white" dangerouslySetInnerHTML={{ __html: description }} />  */}
-        {dangerousHtml ? (
+        <p className="relative text-white w-full h-fit">
+       {hiddenText ? truncatedText : description}
+        {shouldTruncate && hiddenText && "..."} 
+        </p>
+        {hiddenText && (
+       
+          <button
+          onClick={() => setHiddenText(!hiddenText)}
+          className="text-blue-500 absolute right-0 bottom-0 text-xs bg-green-300 h-[30%] hover:text-blue-700 focus:outline-none whitespace-nowrap"
+        >
+          {hiddenText && "Read More"}
+        </button>
+        
+
+      )}
+      
+        {/* {dangerousHtml ? (
           <div
-            className="text-white"
+            className="text-white overflow-hidden"
             dangerouslySetInnerHTML={{ __html: description }}
           />
         ) : (
-          <p className="text-white">{description}</p>
-        )}
+          <p className="text-white overflow-hidden">{description}</p>
+        )} */}
       </div>
-      <div className="p-0 m-0 flex items-center justify-end">
-        <button className="text-white bg-green-500">
-          {hiddenText ? "...Read more" : "Show less"}
-        </button>
-      </div>
+      
+        {/* <button className="absolute top-[10.2rem] right-2 text-white bg-green-500">
+          {hiddenText && "Read more"}
+        </button> */}
+      
     </div>
   );
 };
